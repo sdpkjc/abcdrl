1ad3807d48d035a34ffc145aeb7793ccefd6929a
Adam Zhao <pazyx728@gmail.com>
2022-11-20 19:41:42 +0800

    Script add wandb tag (#10)
    * tentative modify the script of benchmark
    
    * Improve benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,60 +1,63 @@
 from __future__ import annotations
 
-import argparse
-import copy
+import itertools
 import subprocess
-import threading
-from distutils.util import strtobool
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, Optional, Union
 
+import fire
 import tomli
 
 
-def parse_args() -> argparse.Namespace:
-    # fmt: off
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--alg", type=str, default="dqn",
-        help="the algorithm of this experiment")
-    parser.add_argument("--env-id", type=str, default="CartPole-v1",
-        help="the id of the environment")
-    parser.add_argument("--device", type=str, default='auto',
-        help="device of the experiment")
-    parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
-        help="if toggled, this experiment will be tracked with Weights and Biases")
-    parser.add_argument("--wandb-project-name", type=str, default="abcdrl",
-        help="the wandb's project name")
-    parser.add_argument("--wandb-entity", type=str, default=None,
-        help="the entity (team) of wandb's project")
-    parser.add_argument("--capture-video", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
-        help="whether to capture videos of the agent performances (check out `videos` folder)")
-    parser.add_argument("--seeds", type=int, action='append', nargs='*', default=[1],
-        help="seed of the experiment")
-
-    args = parser.parse_args()
-    # fmt: on
-    if len(args.seeds) > 1:
-        args.seeds = args.seeds[1]
-    return args
-
-
-def train_process(cmd, kwargs):
+def train_process(alg: str, env_id: str, kwargs: dict[str, Any]) -> None:
+    with open("benchmark.toml", "rb") as file:
+        cmd = tomli.load(file)[alg][env_id]
     for param in kwargs.items():
-        cmd += f" --{param[0].replace('_','-')} {param[1]}"
+        if isinstance(param[1], list):
+            cmd += f' --{param[0]} "{param[1]}"'
+        else:
+            cmd += f" --{param[0]} {param[1]}"
     subprocess.run(cmd, shell=True, check=True)
 
 
+def main(
+    algs: Union[str, list[str]] = ["dqn"],
+    env_ids: Union[str, list[str]] = ["CartPole-v1"],
+    seeds: Union[str, list[int]] = [1],
+    device: str = "auto",
+    workers: int = 3,
+    track: bool = False,
+    wandb_project_name: str = "abcdrl",
+    wandb_entity: Optional[str] = None,
+    wandb_tags: Union[str, list[str]] = [],
+    capture_video: bool = False,
+):
+    if not isinstance(algs, list):
+        algs = [algs]
+    if not isinstance(env_ids, list):
+        env_ids = [env_ids]
+    if not isinstance(seeds, list):
+        seeds = [seeds]
+    if not isinstance(wandb_tags, list):
+        wandb_tags = [wandb_tags]
+
+    git_commit_sha = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
+    wandb_tags.append(git_commit_sha)
+
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        for element in itertools.product(algs, env_ids, seeds):
+            alg, env_id, seed = element
+            kwargs = {
+                "device": device,
+                "track": track,
+                "wandb-project-name": wandb_project_name,
+                "wandb-entity": wandb_entity,
+                "wandb-tags": wandb_tags,
+                "capture-video": capture_video,
+                "seed": seed,
+            }
+            executor.submit(train_process, alg, env_id, kwargs)
+
+
 if __name__ == "__main__":
-    args = parse_args()
-
-    kwargs = vars(args)
-    with open("benchmark.toml", "rb") as file:
-        cmd = tomli.load(file)[kwargs["alg"]][kwargs["env_id"]]
-
-    kwargs.pop("alg")
-    kwargs.pop("env_id")
-    seeds = kwargs.pop("seeds")
-
-    for seed in seeds:
-        kwargs_tmp = copy.deepcopy(kwargs)
-        kwargs_tmp["seed"] = seed
-        thread = threading.Thread(target=train_process, args=(cmd, kwargs_tmp))
-        thread.start()
+    fire.Fire(main)
