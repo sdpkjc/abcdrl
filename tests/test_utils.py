d4126a1ac4ee2d40377b289705fae9406e58a5c6
Adam Zhao <pazyx728@gmail.com>
2022-11-22 20:23:34 +0800

    fix type-hint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -65,16 +65,16 @@ def test_example_eval_model() -> None:
 def set_all_wrappers(
     func: Callable[..., Generator[dict[str, Any], None, None]]
 ) -> Callable[..., Generator[dict[str, Any], None, None]]:
-    func = abcdrl_utils.wrapper_eval_step(func)
-    func = abcdrl_utils.wrapper_logger(func)
-    func = abcdrl_utils.wrapper_save_model(func)
-    func = abcdrl_utils.wrapper_print_filter(func)
+    func = abcdrl_utils.wrapper_eval_step(func)  # type: ignore[assignment]
+    func = abcdrl_utils.wrapper_logger(func)  # type: ignore[assignment]
+    func = abcdrl_utils.wrapper_save_model(func)  # type: ignore[assignment]
+    func = abcdrl_utils.wrapper_print_filter(func)  # type: ignore[assignment]
     return func
 
 
 def test_dqn_wrappers() -> None:
     Trainer = abcdrl.dqn.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="CartPole-v1",
         device="auto",
@@ -84,13 +84,13 @@ def test_dqn_wrappers() -> None:
         buffer_size=10,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_ddqn_wrappers() -> None:
     Trainer = abcdrl.ddqn.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="CartPole-v1",
         device="auto",
@@ -100,13 +100,13 @@ def test_ddqn_wrappers() -> None:
         buffer_size=10,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_pdqn_wrappers() -> None:
     Trainer = abcdrl.pdqn.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="CartPole-v1",
         device="auto",
@@ -116,13 +116,13 @@ def test_pdqn_wrappers() -> None:
         buffer_size=10,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_ddpg_wrappers() -> None:
     Trainer = abcdrl.ddpg.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="Hopper-v4",
         device="auto",
@@ -132,13 +132,13 @@ def test_ddpg_wrappers() -> None:
         buffer_size=32,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_td3_wrappers() -> None:
     Trainer = abcdrl.td3.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="Hopper-v4",
         device="auto",
@@ -148,13 +148,13 @@ def test_td3_wrappers() -> None:
         buffer_size=32,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_sac_wrappers() -> None:
     Trainer = abcdrl.sac.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="Hopper-v4",
         device="auto",
@@ -164,13 +164,13 @@ def test_sac_wrappers() -> None:
         buffer_size=32,
         batch_size=4,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
 
 
 def test_ppo_wrappers() -> None:
     Trainer = abcdrl.ppo.Trainer
-    Trainer.__call__ = set_all_wrappers(Trainer.__call__)
+    Trainer.__call__ = set_all_wrappers(Trainer.__call__)  # type: ignore[assignment]
     trainer = Trainer(
         env_id="Hopper-v4",
         device="auto",
@@ -179,5 +179,5 @@ def test_ppo_wrappers() -> None:
         num_minibatches=16,
         total_timesteps=256,
     )
-    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):
+    for _ in trainer(eval_frequency=5, num_steps_eval=1, save_frequency=16):  # type: ignore[call-arg]
         pass
