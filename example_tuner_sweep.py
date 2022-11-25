da5e3b2583ea0f97460986956786e6086d512c8b
Adam Zhao <pazyx728@gmail.com>
2022-11-25 21:09:20 +0800

    fix tuner example

--- example_tuner_sweep.py
+++ example_tuner_sweep.py
@@ -23,33 +23,34 @@ sweep_configuration = {
 
 
 def tune_agent() -> None:
-    writer = wandb.init(name=f"{sweep_configuration['name']}__{int(time.time())}")
-    wandb.save("abcdrl/dqn.py")
+    with wandb.init(name=f"{sweep_configuration['name']}__{int(time.time())}") as writer:  # type: ignore[union-attr]
+        wandb.save("abcdrl/dqn.py")
 
-    trainer = abcdrl.dqn.Trainer(
-        env_id=wandb.config.env_id,
-        learning_rate=wandb.config.learning_rate,
-        batch_size=wandb.config.batch_size,
-        train_frequency=wandb.config.train_frequency,
-        target_network_frequency=wandb.config.target_network_frequency,
-        exploration_fraction=wandb.config.exploration_fraction,
-    )
+        trainer = abcdrl.dqn.Trainer(
+            env_id=wandb.config.env_id,
+            learning_rate=wandb.config.learning_rate,
+            batch_size=wandb.config.batch_size,
+            train_frequency=wandb.config.train_frequency,
+            target_network_frequency=wandb.config.target_network_frequency,
+            exploration_fraction=wandb.config.exploration_fraction,
+        )
 
-    for log_data in trainer():
-        if "logs" in log_data:
-            if log_data["log_type"] != "train":
-                print(log_data)
-            writer.log(
-                {f"{log_data['log_type']}/{x[0]}": x[1] for x in log_data["logs"].items()}, step=log_data["sample_step"]
-            )
-            writer.log({"global_step": log_data["sample_step"]}, step=log_data["sample_step"])
+        for log_data in trainer():
+            if "logs" in log_data:
+                if log_data["log_type"] != "train":
+                    print(log_data)
+                writer.log(
+                    {f"{log_data['log_type']}/{x[0]}": x[1] for x in log_data["logs"].items()},
+                    step=log_data["sample_step"],
+                )
+                writer.log({"global_step": log_data["sample_step"]}, step=log_data["sample_step"])
 
 
 def main(
     sweep_id: str | None = None, run_count: int = 1, wandb_project_name: str = "abcdrl", wandb_entity: str | None = None
 ):
     if sweep_id is None:
-        sweep_id = wandb.sweep(sweep=sweep_configuration, project=wandb_project_name, entity=wandb_entity)
+        sweep_id = wandb.sweep(sweep=sweep_configuration, project=wandb_project_name, entity=wandb_entity)  # type: ignore[arg-type]
     wandb.agent(sweep_id, function=tune_agent, count=run_count)
 
 
