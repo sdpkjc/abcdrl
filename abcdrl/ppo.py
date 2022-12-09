19021363e0ecbb2b8f6581cbc77cbf85a2c1b072
Adam Zhao <pazyx728@gmail.com>
2022-12-09 16:07:26 +0800

    fix

--- abcdrl/ppo.py
+++ abcdrl/ppo.py
@@ -419,9 +419,8 @@ class Trainer:
             for idx, final_obs in enumerate(infos["final_observation"]):
                 if final_obs is not None:
                     real_next_obs[idx] = final_obs
-                    if not next_terminated[idx] and next_truncated[idx]:
-                        _, _, terminal_value = self.agent.sample(np.expand_dims(real_next_obs[idx], axis=0))
-                        reward[idx] += self.kwargs["gamma"] * terminal_value
+                    _, _, terminal_value = self.agent.sample(np.expand_dims(real_next_obs[idx], axis=0))
+                    reward[idx] += self.kwargs["gamma"] * (1 - next_terminated[idx]) * terminal_value
 
         self.buffer.add(self.obs, act, reward, self.terminated, val, log_prob)
         if self.buffer.full:
