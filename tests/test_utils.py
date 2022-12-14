32cda34fb0f9ca62e689fbe16dbc369c62cf6b26
Adam Zhao <pazyx728@gmail.com>
2022-12-15 03:12:05 +0800

    add test_capture_video()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -54,3 +54,20 @@ def test_example_eval_model() -> None:
         shell=True,
         check=True,
     )
+
+
+def test_capture_video() -> None:
+    subprocess.run(
+        "python abcdrl/dqn.py"
+        + " --env-id CartPole-v1"
+        + " --device auto"
+        + " --num-envs 2"
+        + " --learning-starts 8"
+        + " --total-timesteps 32"
+        + " --buffer-size 10"
+        + " --batch-size 4"
+        + " --capture-video True",
+        shell=True,
+        check=True,
+        timeout=100,
+    )
