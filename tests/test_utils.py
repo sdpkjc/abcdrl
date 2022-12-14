0fac036a656b5f7ada8f59f117bcc50bde21668f
Adam Zhao <pazyx728@gmail.com>
2022-12-15 01:57:38 +0800

    add wandb track test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -54,3 +54,47 @@ def test_example_eval_model() -> None:
         shell=True,
         check=True,
     )
+
+
+def test_capture_video() -> None:
+    subprocess.run(
+        "python abcdrl/dqn.py"
+        + " --env-id CartPole-v1"
+        + " --device auto"
+        + " --num-envs 2"
+        + " --learning-starts 8"
+        + " --total-timesteps 32"
+        + " --buffer-size 10"
+        + " --batch-size 4"
+        + " --capture-video True",
+        shell=True,
+        check=True,
+        timeout=100,
+    )
+
+
+def test_wandb_track() -> None:
+    online_flag = False
+    re = subprocess.run("wandb status", shell=True, check=True, capture_output=True)
+    if "disabled" not in str(re.stdout):
+        subprocess.run("wandb offline", shell=True, check=True)
+        online_flag = True
+
+    try:
+        subprocess.run(
+            "python abcdrl/dqn.py"
+            + " --env-id CartPole-v1"
+            + " --device auto"
+            + " --num-envs 2"
+            + " --learning-starts 8"
+            + " --total-timesteps 32"
+            + " --buffer-size 10"
+            + " --batch-size 4"
+            + " --capture-video True",
+            shell=True,
+            check=True,
+            timeout=100,
+        )
+    finally:
+        if online_flag:
+            subprocess.run("wandb online", shell=True, check=True)
