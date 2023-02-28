eb4c9c603874dc9e510399ba1142d03f22f8341e
Adam Zhao <pazyx728@gmail.com>
2023-02-28 19:57:55 +0800

    example tests

--- tests/test_examples_torch.py
+++ tests/test_examples_torch.py
@@ -6,15 +6,15 @@ import subprocess
 def test_example_dqn_all_wrapper_torch() -> None:
     subprocess.run(
         "python abcdrl/utils/dqn_all_wrappers_torch.py"
-        + " --env-id CartPole-v1"
-        + " --num-envs 2"
-        + " --learning-starts 8"
-        + " --total-timesteps 32"
-        + " --buffer-size 10"
-        + " --batch-size 4"
-        + " --eval-frequency 5"
-        + " --num-steps-eval 1"
-        + " --save-frequency 16",
+        + " --trainer.env-id CartPole-v1"
+        + " --trainer.num-envs 2"
+        + " --trainer.learning-starts 8"
+        + " --trainer.total-timesteps 32"
+        + " --trainer.buffer-size 10"
+        + " --trainer.batch-size 4"
+        + " --trainer.eval-frequency 5"
+        + " --trainer.num-steps-eval 1"
+        + " --saver.save-frequency 16",
         shell=True,
         check=True,
         timeout=100,
@@ -24,14 +24,14 @@ def test_example_dqn_all_wrapper_torch() -> None:
 def test_example_eval_model_torch() -> None:
     subprocess.run(
         "python abcdrl/utils/dqn_all_wrappers_torch.py"
-        + " --exp_name test_eval_dqn"
-        + " --env-id CartPole-v1"
-        + " --num-envs 1"
-        + " --learning-starts 8"
-        + " --total-timesteps 32"
-        + " --buffer-size 64"
-        + " --batch-size 4"
-        + " --save-frequency 16",
+        + " --trainer.exp_name test_eval_dqn"
+        + " --trainer.env-id CartPole-v1"
+        + " --trainer.num-envs 1"
+        + " --trainer.learning-starts 8"
+        + " --trainer.total-timesteps 32"
+        + " --trainer.buffer-size 64"
+        + " --trainer.batch-size 4"
+        + " --saver.save-frequency 16",
         shell=True,
         check=True,
         timeout=100,
